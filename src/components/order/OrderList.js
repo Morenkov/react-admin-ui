7f14997fec0304f7ebd3580ec6419a43ddb86a40
Aleksandr Maleta <a.maleta@dats.team>
2023-06-11 15:55:49 +0200

    added filter

--- src/components/order/OrderList.js
+++ src/components/order/OrderList.js
@@ -1,9 +1,22 @@
 import React from 'react'
-import {Datagrid, DateField, DeleteButton, EditButton, List, NumberField, TextField,} from 'react-admin'
+import {
+  Datagrid,
+  DateField,
+  DeleteButton,
+  EditButton,
+  FilterList,
+  FilterListItem,
+  FilterLiveSearch,
+  List,
+  NumberField,
+  SavedQueriesList,
+  TextField,
+} from 'react-admin'
+import {Card, CardContent} from "@mui/material";
 
 const OrderList = (props) => {
   return (
-    <List {...props}>
+    <List aside={<PostFilterSidebar />} {...props}>
       <Datagrid>
         <NumberField source='id' />
         <NumberField multiline source='parcelId'/>
@@ -18,4 +31,19 @@ const OrderList = (props) => {
   )
 }
 
+export const PostFilterSidebar = () => (
+    <Card sx={{ order: -1, mr: 2, mt: 9, width: 200 }}>
+      <CardContent>
+        <SavedQueriesList />
+        <FilterLiveSearch />
+        <FilterList label="Fields" >
+          <FilterListItem label="Parsel" value={{ order: 'parcelId' }} />
+          <FilterListItem label="Driver" value={{ order: 'customerId' }} />
+          <FilterListItem label="Customer" value={{ order: 'customerId' }} />
+          <FilterListItem label="OrderDate" value={{ order: 'orderDate' }} />
+          <FilterListItem label="Status" value={{ order: 'status' }} />
+        </FilterList>
+      </CardContent>
+    </Card>
+);
 export default OrderList
